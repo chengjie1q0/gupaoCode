7a80c5c23cf282ee725315a6102a9d48a14a2a6b
380231282@qq.com <380231282@qq.com>
2018-08-13 14:30:34 +0800

    add delegate

---
+++ gupaoedu-vip-proxy/src/com/gupaoedu/vip/delegate/ExectorA.java
@@ -0,0 +1,11 @@
+package com.gupaoedu.vip.delegate;
+
+public class ExectorA implements IExector{
+
+	@Override
+	public void doing() {
+		System.out.println("员工A开始执行任务");
+		
+	}
+
+}
---
+++ gupaoedu-vip-proxy/src/com/gupaoedu/vip/delegate/ExectorB.java
@@ -0,0 +1,11 @@
+package com.gupaoedu.vip.delegate;
+
+public class ExectorB implements IExector{
+
+	@Override
+	public void doing() {
+		System.out.println("员工B开始执行任务");
+		
+	}
+
+}
